9e8cd2406cf5596cb15e78fe9120cada88963312
joellefkowitz <joellefkowitz@hotmail.com>
2021-08-13 17:04:53 +0100

    Add logfile options

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM python:3.8
-
 WORKDIR /imapper
-COPY . .
 
+COPY setup.py setup.cfg ./
+RUN mkdir -p src
 RUN pip install .
+
+COPY . .
 ENTRYPOINT [ "python", "-m", "src" ]
+ 
--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,7 @@
+import os
 import json
 import logging
+from pathlib import Path
 from inspect import cleandoc
 from logging.handlers import TimedRotatingFileHandler
 
@@ -11,28 +13,37 @@ from .sweeper import sweep_on_loop
 
 logger = logging.getLogger()
 
-logging.basicConfig(
-    format="%(asctime)s %(levelname)-8s %(message)s",
-    level=logging.INFO,
-    datefmt="%Y-%m-%d %H:%M:%S",
-    handlers=[TimedRotatingFileHandler("imapper.log", "h", 1)],
-)
-
 cli = cleandoc(
     """
     Imapper: IMAP message sweeper.
 
     Usage:
-    imapper (sweep|senders) PATH
+    imapper (sweep|senders) PATH --log=<logfile>
 
     Options:
-    -h --help     Show this screen.
+    -h --help   Show this screen.
     """
 )
 
 
 def main() -> None:
     args = docopt(cli)
+
+    print(args)
+
+    if args["--log"]:
+        logfile = args["--log"]
+    else:
+        logfile = os.path.join("logs", "imapper.log")
+
+    Path(os.path.dirname(logfile)).mkdir(parents=True, exist_ok=True)
+
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        level=logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=[TimedRotatingFileHandler(logfile, "h", 1)],
+    )
 
     with open(args["PATH"]) as stream:
         clients = parse_clients(json.load(stream))
